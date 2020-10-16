2f74d187ac7eea073a688f53e2b2b80d9420b739
Harikrishnan Balagopal <Harikrishnan.Balagopal@ibm.com>
2020-10-16 17:20:32 +0530

    Added prompt before proceeding to install dependencies
    Signed-off-by: Harikrishnan Balagopal <Harikrishnan.Balagopal@ibm.com>

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -15,6 +15,9 @@
 #   limitations under the License.
 
 # initOS discovers the operating system for this system.
+
+MOVE2KUBE_DEP_INSTALL_PATH="$PWD/bin"
+
 initOS() {
     OS="$(uname | tr '[:upper:]' '[:lower:]')"
 
@@ -23,6 +26,16 @@ initOS() {
     mingw*) OS='windows' ;;
     esac
 }
+
+askBeforeProceeding() {
+    echo 'Move2Kube dependency installation script.'
+    echo 'The following dependencies will be installed to '"$MOVE2KUBE_DEP_INSTALL_PATH"':'
+    echo 'docker (only on Linux), pack, kubectl, operator-sdk'
+    echo "$MOVE2KUBE_DEP_INSTALL_PATH"' will be added to the $PATH'
+    read -r -p 'Proceed? [y/N]:' oktoproceed
+}
+
+# MAIN
 
 initOS
 
@@ -35,19 +48,30 @@ elif [ "$OS" == "linux" ]; then
     PACKPLATFORM='linux'
     KUBECTLPLATFORM='linux'
     OPERATORSDKPLATFORM='linux-gnu'
-    if ! grep -q container_t '/proc/1/attr/current'; then
-        curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh
-    fi
 else
-    echo 'Unsupported platform.'
+    echo 'Unsupported platform:'"$OS"' . Exiting.'
     exit 1
 fi
 
-mkdir -p bin
-curl -o pack.tgz -LJO 'https://github.com/buildpacks/pack/releases/download/v0.12.0/pack-v0.12.0-'"$PACKPLATFORM"'.tgz' && tar -xzf pack.tgz && mv pack bin/ && rm pack.tgz
-curl -o kubectl -LJO 'https://storage.googleapis.com/kubernetes-release/release/'"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"'/bin/'"$KUBECTLPLATFORM"'/amd64/kubectl' && mv kubectl bin/
-chmod +x bin/kubectl
-curl -o operator-sdk -LJO 'https://github.com/operator-framework/operator-sdk/releases/download/v1.0.0/operator-sdk-v1.0.0-x86_64-'"$OPERATORSDKPLATFORM" && mv operator-sdk bin/
-chmod +x bin/operator-sdk
-echo 'PATH="$PATH:'"$PWD"'/bin"' >>~/.bash_profile
-echo 'We have added a line to your '~/.bash_profile' to put '"$PWD/bin"' on your $PATH. Either restart the shell or source ~/.bash_profile to see the changes.'
+askBeforeProceeding
+
+if [ "$oktoproceed" != 'y' ]; then
+    echo 'Failed to get confirmation. Exiting.'
+    exit 1
+fi
+
+if [ "$OS" == "linux" ]; then
+    if ! grep -q container_t '/proc/1/attr/current'; then
+        curl -fsSL 'https://get.docker.com' -o 'get-docker.sh' && sudo sh 'get-docker.sh' && rm 'get-docker.sh'
+    fi
+fi
+
+mkdir -p "$MOVE2KUBE_DEP_INSTALL_PATH"
+curl -o pack.tgz -LJO 'https://github.com/buildpacks/pack/releases/download/v0.12.0/pack-v0.12.0-'"$PACKPLATFORM"'.tgz' && tar -xzf pack.tgz && mv pack "$MOVE2KUBE_DEP_INSTALL_PATH" && rm pack.tgz
+curl -o kubectl -LJO 'https://storage.googleapis.com/kubernetes-release/release/'"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"'/bin/'"$KUBECTLPLATFORM"'/amd64/kubectl' && mv kubectl "$MOVE2KUBE_DEP_INSTALL_PATH"
+chmod +x "$MOVE2KUBE_DEP_INSTALL_PATH"/kubectl
+curl -o operator-sdk -LJO 'https://github.com/operator-framework/operator-sdk/releases/download/v1.0.0/operator-sdk-v1.0.0-x86_64-'"$OPERATORSDKPLATFORM" && mv operator-sdk "$MOVE2KUBE_DEP_INSTALL_PATH"
+chmod +x "$MOVE2KUBE_DEP_INSTALL_PATH"/operator-sdk
+echo 'PATH="$PATH:'"$MOVE2KUBE_DEP_INSTALL_PATH"'"' >>~/.bash_profile
+echo 'Installed the dependencies to '"$MOVE2KUBE_DEP_INSTALL_PATH"
+echo 'We have added a line to your '~/.bash_profile' to put '"$MOVE2KUBE_DEP_INSTALL_PATH"' on your $PATH. Either restart the shell or source ~/.bash_profile to see the changes.'
