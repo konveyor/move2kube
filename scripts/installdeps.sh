b7693c40599d73896e977e1c70bc5d573fcc2a09
Harikrishnan Balagopal <Harikrishnan.Balagopal@ibm.com>
2020-10-16 15:58:36 +0530

    Make script pass shellcheck. Avoid common bash pitfalls.
    Signed-off-by: Harikrishnan Balagopal <Harikrishnan.Balagopal@ibm.com>

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -14,28 +14,40 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-if [ "$(uname)" == "Darwin" ]; then
-    PACKPLATFORM="macos"
-    KUBECTLPLATFORM="darwin"
-    OPERATORSDKPLATFORM="apple-darwin"
-    echo "Once this installation finishes, please do install docker from https://docs.docker.com/docker-for-mac/install/"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    PACKPLATFORM="linux"
-    KUBECTLPLATFORM="linux"
-    OPERATORSDKPLATFORM="linux-gnu"
-    if ! grep -q container_t "/proc/1/attr/current"; then
+# initOS discovers the operating system for this system.
+initOS() {
+    OS="$(uname | tr '[:upper:]' '[:lower:]')"
+
+    case "$OS" in
+    # Minimalist GNU for Windows
+    mingw*) OS='windows' ;;
+    esac
+}
+
+initOS
+
+if [ "$OS" == "darwin" ]; then
+    PACKPLATFORM='macos'
+    KUBECTLPLATFORM='darwin'
+    OPERATORSDKPLATFORM='apple-darwin'
+    echo 'Once this installation finishes, please do install docker from https://docs.docker.com/docker-for-mac/install/'
+elif [ "$OS" == "linux" ]; then
+    PACKPLATFORM='linux'
+    KUBECTLPLATFORM='linux'
+    OPERATORSDKPLATFORM='linux-gnu'
+    if ! grep -q container_t '/proc/1/attr/current'; then
         curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh
     fi
 else
-    echo "Unsupported platform."
+    echo 'Unsupported platform.'
     exit 1
 fi
 
 mkdir -p bin
-curl -o pack.tgz -LJO https://github.com/buildpacks/pack/releases/download/v0.12.0/pack-v0.12.0-$PACKPLATFORM.tgz && tar -xzf pack.tgz && mv pack bin/ && rm pack.tgz
-curl -o kubectl -LJO https://storage.googleapis.com/kubernetes-release/release/"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"/bin/$KUBECTLPLATFORM/amd64/kubectl && mv kubectl bin/
+curl -o pack.tgz -LJO 'https://github.com/buildpacks/pack/releases/download/v0.12.0/pack-v0.12.0-'"$PACKPLATFORM"'.tgz' && tar -xzf pack.tgz && mv pack bin/ && rm pack.tgz
+curl -o kubectl -LJO 'https://storage.googleapis.com/kubernetes-release/release/'"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"'/bin/'"$KUBECTLPLATFORM"'/amd64/kubectl' && mv kubectl bin/
 chmod +x bin/kubectl
-curl -o operator-sdk -LJO https://github.com/operator-framework/operator-sdk/releases/download/v1.0.0/operator-sdk-v1.0.0-x86_64-$OPERATORSDKPLATFORM && mv operator-sdk bin/
+curl -o operator-sdk -LJO 'https://github.com/operator-framework/operator-sdk/releases/download/v1.0.0/operator-sdk-v1.0.0-x86_64-'"$OPERATORSDKPLATFORM" && mv operator-sdk bin/
 chmod +x bin/operator-sdk
-echo "PATH=$PATH:$PWD/bin" >> ~/.bash_profile
-source ~/.bash_profile
+echo 'PATH="$PATH:'"$PWD"'/bin"' >>~/.bash_profile
+echo 'We have added a line to your '~/.bash_profile' to put '"$PWD/bin"' on your $PATH. Either restart the shell or source ~/.bash_profile to see the changes.'
