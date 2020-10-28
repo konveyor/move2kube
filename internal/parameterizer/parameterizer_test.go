aa6330223792103b8031ac291b8761a6f0fcabf5
Pablo Loyola <pablo.loyola.h@gmail.com>
2020-10-29 01:25:35 +0900

    parameterizer basic tests -2, updated test cases after review
    Signed-off-by: Pablo Loyola <pablo.loyola.h@gmail.com>

--- internal/parameterizer/parameterizer_test.go
+++ internal/parameterizer/parameterizer_test.go
@@ -17,7 +17,6 @@ limitations under the License.
 package parameterize_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp/cmpopts"
@@ -28,6 +27,7 @@ import (
 	parameterize "github.com/konveyor/move2kube/internal/parameterizer"
 	"github.com/konveyor/move2kube/internal/types"
 	irtypes "github.com/konveyor/move2kube/internal/types"
+	"github.com/konveyor/move2kube/types/output"
 	plantypes "github.com/konveyor/move2kube/types/plan"
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
@@ -39,48 +39,58 @@ func TestParameterizer(t *testing.T) {
 	t.Run("1.IR with no services, no storage", func(t *testing.T) {
 
 		ir := getIRWithoutServices()
-		irAfter, err := parameterize.Parameterize(ir)
+		want := getIRWithoutServices()
+		actual, err := parameterize.Parameterize(ir)
 
 		if err != nil {
-			t.Fatal("Failed to parameterize. Error:", err)
+			t.Fatal("Failed to parameterize the IR properly. Error:", err)
 		}
 
-		irAfter.TargetClusterSpec.Host = ""
+		actual.TargetClusterSpec.Host = ""
 
-		if !cmp.Equal(ir, irAfter, cmpopts.EquateEmpty()) {
-			t.Fatalf("TODO explanation. Difference:\n%s", cmp.Diff(ir, irAfter))
+		if !cmp.Equal(actual, want, cmpopts.EquateEmpty()) {
+			t.Fatalf("Failed to parameterize the IR properly. Difference:\n%s", cmp.Diff(want, actual))
 		}
 
 	})
 
-	/*
-		t.Run("2.IR containing services that have no containers", func(t *testing.T) {
+	t.Run("2.IR containing services that have no containers", func(t *testing.T) {
 
-			ir := getIRWithServicesAndWithoutContainers()
-			irAfter, err := parameterize.Parameterize(ir)
-			if err != nil {
-				t.Fatal("Failed to parameterize. Error:", err)
-			}
+		ir := getIRWithServicesAndWithoutContainers()
+		want := getIRWithServicesAndWithoutContainers()
+		actual, err := parameterize.Parameterize(ir)
+		if err != nil {
+			t.Fatal("Failed to parameterize the IR properly. Error:", err)
+		}
 
-			// oracle
-			// if the service has no containers, then no changes will be done
-			// ir = irAfter
-			if !cmp.Equal(ir, irAfter, cmpopts.EquateEmpty()) {
-				t.Fatalf("TODO explanation. Difference:\n%s", cmp.Diff(ir, irAfter))
-			}
+		want.Values.Services = map[string]output.Service{
+			"svcname1": {Containers: map[string]output.Container{}},
+			"svcname2": {Containers: map[string]output.Container{}},
+		}
+		actual.TargetClusterSpec.Host = ""
 
-		})
-	*/
+		if !cmp.Equal(actual, want, cmpopts.EquateEmpty()) {
+			t.Fatalf("Failed to parameterize the IR properly. Difference:\n%s", cmp.Diff(want, actual))
+		}
+
+	})
 
 	t.Run("3.IR containing services with containers", func(t *testing.T) {
 
 		ir := getIRWithServicesAndContainers()
-		irAfter, err := parameterize.Parameterize(ir)
+		want := getIRWithServicesAndContainers()
+		actual, err := parameterize.Parameterize(ir)
 		if err != nil {
-			t.Fatal("Failed to parameterize. Error:", err)
+			t.Fatal("Failed to parameterize the IR properly. Error:", err)
 		}
-		fmt.Printf("%T", irAfter)
-		// oracle
+
+		want.TargetClusterSpec.Host = "{{ .Release.Name }}-{{ .Values.ingresshost }}"
+		want.Services["svcname1"].PodSpec.Containers[0].Image = `:{{ index .Values.services "svcname1" "containers" "container-1" "imagetag"  }}`
+		want.Values.Services = map[string]output.Service{"svcname1": {Containers: map[string]output.Container{"container-1": {TagName: "latest"}}}}
+
+		if !cmp.Equal(actual, want, cmpopts.EquateEmpty()) {
+			t.Fatalf("Failed to parameterize the IR properly. Difference:\n%s", cmp.Diff(want, actual))
+		}
 
 	})
 
@@ -89,16 +99,16 @@ func TestParameterizer(t *testing.T) {
 		// as the type is not PVCKind, the scMap will be empty
 		// -> no parameterizations -> before = after
 		ir := getIRWithStorageNotPVCKind()
-		irAfter, err := parameterize.Parameterize(ir)
+		want := getIRWithStorageNotPVCKind()
+		actual, err := parameterize.Parameterize(ir)
 		if err != nil {
-			t.Fatal("Failed to parameterize. Error:", err)
+			t.Fatal("Failed to parameterize the IR properly. Error:", err)
 		}
 
 		// oracle
-
-		irAfter.TargetClusterSpec.Host = ""
-		if !cmp.Equal(ir, irAfter, cmpopts.EquateEmpty()) {
-			t.Fatalf("TODO explanation. Difference:\n%s", cmp.Diff(ir, irAfter))
+		actual.TargetClusterSpec.Host = ""
+		if !cmp.Equal(actual, want, cmpopts.EquateEmpty()) {
+			t.Fatalf("Failed to parameterize the IR properly. Difference:\n%s", cmp.Diff(want, actual))
 		}
 
 	})
@@ -107,34 +117,37 @@ func TestParameterizer(t *testing.T) {
 
 		// scMap should be 1, we can parameterize
 		ir := getIRWithStoragePVCKind()
-		irAfter, err := parameterize.Parameterize(ir)
+		want := getIRWithStoragePVCKind()
+		actual, err := parameterize.Parameterize(ir)
 		if err != nil {
-			t.Fatal("Failed to parameterize. Error:", err)
+			t.Fatal("Failed to parameterize the IR properly. Error:", err)
 		}
 
-		// oracle:
 		paramSC := "{{ .Values.storageclass }}"
-		for _, j := range irAfter.Storages {
-			x := *j.PersistentVolumeClaimSpec.StorageClassName
-			if x != paramSC {
-				t.Fatal("Failed to parameterize. Error:", err)
-			}
+		want.Storages[0].PersistentVolumeClaimSpec.StorageClassName = &paramSC
+		want.TargetClusterSpec.Host = "{{ .Release.Name }}-{{ .Values.ingresshost }}"
+		want.Values.Services = map[string]output.Service{}
+		want.Values.StorageClass = "storage-1cn"
+
+		if !cmp.Equal(actual, want, cmpopts.EquateEmpty()) {
+			t.Fatalf("Failed to parameterize the IR properly. Difference:\n%s", cmp.Diff(want, actual))
 		}
+
 	})
 
 	t.Run("6.IR with no services , check ingress parameterizer ", func(t *testing.T) {
 
 		// scMap should be 1, we can parameterize
 		ir := getIRWithoutServices()
-		irAfter, err := parameterize.Parameterize(ir)
+		actual, err := parameterize.Parameterize(ir)
 		if err != nil {
-			t.Fatal("Failed to parameterize. Error:", err)
+			t.Fatal("Failed to parameterize the IR properly. Error:", err)
 		}
 
-		hostParam := "{{ .Release.Name }}-"
+		hostParam := "{{ .Release.Name }}-{{ .Values.ingresshost }}"
 
-		if irAfter.TargetClusterSpec.Host != hostParam {
-			t.Fatal("Failed to parameterize")
+		if actual.TargetClusterSpec.Host != hostParam {
+			t.Fatal("Failed to parameterize the IR properly")
 
 		}
 	})
@@ -160,24 +173,14 @@ func getIRWithServicesAndWithoutContainers() types.IR {
 }
 
 func getIRWithServicesAndContainers() types.IR {
-	c1 := corev1.Container{
-		Name: "container-1",
-	}
-	c2 := corev1.Container{
-		Name: "container-2",
-	}
-	//c1.ImagePullPolicy = v1.PullAlways // not needed for the moment
-	//c2.ImagePullPolicy = v1.PullAlways // not needed for the moment
+	c1 := corev1.Container{Name: "container-1"}
 	svcname1 := "svcname1"
-	svcname2 := "svcname2"
 	svc1 := types.Service{Name: svcname1, Replicas: 2}
-	svc2 := types.Service{Name: svcname2, Replicas: 4}
 	svc1.Containers = append(svc1.Containers, c1)
-	svc2.Containers = append(svc2.Containers, c2)
 	p := plantypes.NewPlan()
 	ir := types.NewIR(p)
 	ir.Services[svcname1] = svc1
-	ir.Services[svcname2] = svc2
+
 	return ir
 }
 
