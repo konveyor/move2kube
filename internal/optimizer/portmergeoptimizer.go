0dc38856c91d4e580a2fcb198b9041b1a42f980a
Harikrishnan Balagopal <Harikrishnan.Balagopal@ibm.com>
2020-11-11 22:11:57 +0530

    changed portMergeOptimizer to ask a question if the service doesn't have any ports exposed
    Signed-off-by: Harikrishnan Balagopal <Harikrishnan.Balagopal@ibm.com>

--- internal/optimizer/portmergeoptimizer.go
+++ internal/optimizer/portmergeoptimizer.go
@@ -17,7 +17,13 @@ limitations under the License.
 package optimize
 
 import (
+	"fmt"
+	"strconv"
+
+	"github.com/konveyor/move2kube/internal/qaengine"
 	irtypes "github.com/konveyor/move2kube/internal/types"
+	qatypes "github.com/konveyor/move2kube/types/qaengine"
+	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -26,19 +32,91 @@ type portMergeOptimizer struct {
 }
 
 //Optimize uses data from ir containers to fill ir.services
-func (ep portMergeOptimizer) optimize(ir irtypes.IR) (irtypes.IR, error) {
-	for k, scObj := range ir.Services {
-		for csIndex, containerSection := range scObj.Containers {
-			if c, ok := ir.GetContainer(containerSection.Image); ok {
-				for _, exposedPort := range c.ExposedPorts {
-					containerSection.Ports = append(containerSection.Ports, corev1.ContainerPort{ContainerPort: int32(exposedPort)})
-				}
-				scObj.Containers[csIndex] = containerSection
+func (opt *portMergeOptimizer) optimize(ir irtypes.IR) (irtypes.IR, error) {
+	for _, service := range ir.Services {
+		serviceHasNoPorts := true
+		for _, coreV1Container := range service.Containers {
+			if len(coreV1Container.Ports) > 0 {
+				serviceHasNoPorts = false
+				break
 			}
 		}
+		if !serviceHasNoPorts {
+			continue
+		}
 
-		ir.Services[k] = scObj
+		// The service has no ports so we gather some eligible ports and ask a question.
+		log.Debugf("The service %s has no ports", service.Name)
+		portToContainerIdx := opt.gatherPorts(ir, service)
+		if len(portToContainerIdx) == 0 {
+			log.Debugf("There are no eligible ports for the service %s", service.Name)
+			continue
+		}
+		selectedPortToContainerIdx := opt.askQuestion(service, portToContainerIdx)
+		if len(selectedPortToContainerIdx) == 0 {
+			log.Infof("User deselected all ports. Not adding any ports to the service %s", service.Name)
+			continue
+		}
+		opt.exposePorts(service, selectedPortToContainerIdx)
 	}
 
 	return ir, nil
+}
+
+func (*portMergeOptimizer) gatherPorts(ir irtypes.IR, service irtypes.Service) map[int]int {
+	portToContainerIdx := map[int]int{}
+	for coreV1ContainerIdx, coreV1Container := range service.Containers {
+		if irContainer, ok := ir.GetContainer(coreV1Container.Image); ok {
+			for _, exposedPort := range irContainer.ExposedPorts {
+				if oldCoreV1ContainerIdx, ok := portToContainerIdx[exposedPort]; ok {
+					log.Warnf("The port %d is eligible to be exposed by both container %s and container %s",
+						exposedPort, service.Containers[oldCoreV1ContainerIdx].Name, coreV1Container.Name)
+				}
+				portToContainerIdx[exposedPort] = coreV1ContainerIdx
+			}
+		}
+	}
+	return portToContainerIdx
+}
+
+func (*portMergeOptimizer) askQuestion(service irtypes.Service, portToContainerIdx map[int]int) map[int]int {
+	eligiblePorts := []string{}
+	for eligiblePort := range portToContainerIdx {
+		eligiblePorts = append(eligiblePorts, fmt.Sprintf("%d", eligiblePort))
+	}
+	message := fmt.Sprintf("Service %s has no ports. Please select the ports that should be added to it:", service.Name)
+	hint := []string{"If this is a headless service deselect all the ports."}
+	problem, err := qatypes.NewMultiSelectProblem(message, hint, eligiblePorts, eligiblePorts)
+	if err != nil {
+		log.Fatalf("Unable to create problem : %s", err)
+	}
+	problem, err = qaengine.FetchAnswer(problem)
+	if err != nil {
+		log.Fatalf("Unable to fetch answer : %s", err)
+	}
+	eligiblePorts, err = problem.GetSliceAnswer()
+	if err != nil {
+		log.Fatalf("Unable to get answer : %s", err)
+	}
+	if len(eligiblePorts) == 0 {
+		log.Info("All ports deselected.")
+		return nil
+	}
+	selectedPortToContainerIdx := map[int]int{}
+	for _, eligiblePort := range eligiblePorts {
+		selectedPort, err := strconv.Atoi(eligiblePort)
+		if err != nil {
+			log.Debugf("Failed to parse %q as an integer port. Error: %q", eligiblePort, err)
+			continue
+		}
+		selectedPortToContainerIdx[selectedPort] = portToContainerIdx[selectedPort]
+	}
+	return selectedPortToContainerIdx
+}
+
+func (*portMergeOptimizer) exposePorts(service irtypes.Service, portToContainerIdx map[int]int) {
+	for port, coreV1ContainerIdx := range portToContainerIdx {
+		coreV1Port := corev1.ContainerPort{ContainerPort: int32(port)}
+		service.Containers[coreV1ContainerIdx].Ports = append(service.Containers[coreV1ContainerIdx].Ports, coreV1Port)
+	}
 }
