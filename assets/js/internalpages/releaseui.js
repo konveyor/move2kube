7c3a431dbda2c71145b3be54c95602cf43c588ee
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2020-12-31 02:39:37 +0530

    ci: forgot to compile the ts script after previous commit
    Need to `npm run all` after making any changes.
    
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- assets/js/internalpages/releaseui.js
+++ assets/js/internalpages/releaseui.js
@@ -30,7 +30,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const semver = __importStar(require("semver"));
 const rest_1 = require("@octokit/rest");
-const release_info_url = 'https://raw.githubusercontent.com/konveyor/move2kube/feat/releaseui/_data/releaseinfo.json';
+const release_info_url = 'https://raw.githubusercontent.com/konveyor/move2kube/gh-pages/_data/releaseinfo.json';
 const owner = 'konveyor';
 const repo = 'move2kube';
 const owner_repos = [
