b13a560201032fb4bf954e9c34125d68e4722c1b
Ashok Pon Kumar <ashokponkumar@gmail.com>
2021-01-30 15:31:27 +0530

    fix: Fix networkpolicy test
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

--- internal/apiresource/networkpolicy_test.go
+++ internal/apiresource/networkpolicy_test.go
@@ -24,10 +24,10 @@ import (
 	"github.com/konveyor/move2kube/internal/common"
 	irtypes "github.com/konveyor/move2kube/internal/types"
 	plantypes "github.com/konveyor/move2kube/types/plan"
-	"k8s.io/kubernetes/pkg/apis/networking"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	core "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/apis/networking"
 )
 
 func TestGetSupportedKinds(t *testing.T) {
