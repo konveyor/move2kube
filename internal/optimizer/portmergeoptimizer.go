9550140d801b5992185b5501b6ed812e5eed883a
Ashok Pon Kumar <ashokponkumar@gmail.com>
2021-02-03 17:58:00 +0530

    fix: Remove per-service port question (#407)
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

--- internal/optimizer/portmergeoptimizer.go
+++ internal/optimizer/portmergeoptimizer.go
@@ -17,14 +17,9 @@ limitations under the License.
 package optimize
 
 import (
-	"fmt"
-
 	"github.com/konveyor/move2kube/internal/common"
-	"github.com/konveyor/move2kube/internal/qaengine"
 	irtypes "github.com/konveyor/move2kube/internal/types"
-	qatypes "github.com/konveyor/move2kube/types/qaengine"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cast"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -52,12 +47,7 @@ func (opt *portMergeOptimizer) optimize(ir irtypes.IR) (irtypes.IR, error) {
 		if len(portToContainerIdx) == 0 {
 			continue
 		}
-		selectedPortToContainerIdx := opt.askQuestion(service, portToContainerIdx)
-		if len(selectedPortToContainerIdx) == 0 {
-			log.Infof("User deselected all ports. Not adding any ports to the service %s", service.Name)
-			continue
-		}
-		opt.exposePorts(&service, selectedPortToContainerIdx)
+		opt.exposePorts(&service, portToContainerIdx)
 		ir.Services[serviceName] = service
 	}
 
@@ -82,46 +72,11 @@ func (*portMergeOptimizer) gatherPorts(ir irtypes.IR, service irtypes.Service) m
 		if len(service.Containers) == 0 {
 			log.Infof("The service %s has no ports because it has no containers.", service.Name)
 		} else {
-			log.Infof("Could not find any eligibile ports for the service %s . Adding default port %d", service.Name, common.DefaultServicePort)
+			log.Infof("No ports detected for service %s . Adding default port %d", service.Name, common.DefaultServicePort)
 			portToContainerIdx[common.DefaultServicePort] = 0 // the first container index
 		}
 	}
 	return portToContainerIdx
-}
-
-func (*portMergeOptimizer) askQuestion(service irtypes.Service, portToContainerIdx map[int]int) map[int]int {
-	eligiblePorts := []string{}
-	for eligiblePort := range portToContainerIdx {
-		eligiblePorts = append(eligiblePorts, cast.ToString(eligiblePort))
-	}
-	key := common.ConfigServicesKey + common.Delim + service.Name + common.Delim + "ports"
-	message := fmt.Sprintf("Service %s has no ports. Please select the ports that should be added to it:", service.Name)
-	hint := []string{"If this is a headless service deselect all the ports."}
-	problem, err := qatypes.NewMultiSelectProblem(key, message, hint, eligiblePorts, eligiblePorts)
-	if err != nil {
-		log.Fatalf("Unable to create problem : %s", err)
-	}
-	problem, err = qaengine.FetchAnswer(problem)
-	if err != nil {
-		log.Fatalf("Unable to fetch answer : %s", err)
-	}
-	eligiblePorts, err = problem.GetSliceAnswer()
-	if err != nil {
-		log.Fatalf("Unable to get answer : %s", err)
-	}
-	if len(eligiblePorts) == 0 {
-		return nil
-	}
-	selectedPortToContainerIdx := map[int]int{}
-	for _, eligiblePort := range eligiblePorts {
-		selectedPort, err := cast.ToIntE(eligiblePort)
-		if err != nil {
-			log.Debugf("Failed to parse %q as an integer port. Error: %q", eligiblePort, err)
-			continue
-		}
-		selectedPortToContainerIdx[selectedPort] = portToContainerIdx[selectedPort]
-	}
-	return selectedPortToContainerIdx
 }
 
 func (*portMergeOptimizer) exposePorts(service *irtypes.Service, portToContainerIdx map[int]int) {
