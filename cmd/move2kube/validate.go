1d9934cf5bd82c981c94c442465d98d51e7cf32c
Ashok Pon Kumar <ashokponkumar@gmail.com>
2021-02-09 13:54:32 +0530

    feat: Add validate framework to list TODO items after move2kube execution
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

---
+++ cmd/move2kube/validate.go
@@ -0,0 +1,62 @@
+/*
+Copyright IBM Corporation 2020
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"path/filepath"
+
+	cmdcommon "github.com/konveyor/move2kube/cmd/common"
+	"github.com/konveyor/move2kube/internal/move2kube"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type validateFlags struct {
+	srcpath string
+}
+
+func validateHandler(flags validateFlags) {
+	srcpath, err := filepath.Abs(flags.srcpath)
+	if err != nil {
+		log.Fatalf("Failed to make the directory path %q absolute. Error: %q", srcpath, err)
+	}
+	move2kube.PrintValidate(srcpath)
+}
+
+func getValidateCommand() *cobra.Command {
+	must := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+	viper.AutomaticEnv()
+
+	flags := validateFlags{}
+	validateCmd := &cobra.Command{
+		Use:   "validate",
+		Short: "Print all next steps in a directory",
+		Long:  "Print all next steps in a directory",
+		Run:   func(*cobra.Command, []string) { validateHandler(flags) },
+	}
+
+	validateCmd.Flags().StringVarP(&flags.srcpath, cmdcommon.SourceFlag, "s", ".", "Specify directory containing the artifacts created by Move2Kube.")
+
+	must(validateCmd.MarkFlagRequired(cmdcommon.SourceFlag))
+
+	return validateCmd
+}
