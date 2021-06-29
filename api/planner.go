21e5c12579556574474ebc2eb4066d6a4d9a15ec
Ashok Pon Kumar <ashokponkumar@gmail.com>
2021-06-29 11:41:40 +0530

    chore: Update license headers and move artifact types to separate package
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

--- api/planner.go
+++ api/planner.go
@@ -1,18 +1,18 @@
 /*
-Copyright IBM Corporation 2020
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ *  Copyright IBM Corporation 2020, 2021
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 package api
 
@@ -22,6 +22,7 @@ import (
 	"github.com/konveyor/move2kube/internal/transformer"
 	"github.com/konveyor/move2kube/qaengine"
 	plantypes "github.com/konveyor/move2kube/types/plan"
+	transformertypes "github.com/konveyor/move2kube/types/transformer"
 	"github.com/sirupsen/logrus"
 )
 
@@ -112,7 +113,7 @@ func CuratePlan(p plantypes.Plan) plantypes.Plan {
 				logrus.Debugf("Ignoring transformer %+v for service %s due to deselected transformer %s", t, sn, t.Mode)
 				continue
 			}
-			artifactsToUse := []plantypes.ArtifactType{}
+			artifactsToUse := []transformertypes.ArtifactType{}
 			for _, at := range t.ArtifactTypes {
 				if common.IsStringPresent(exclusiveArtifactTypes, string(at)) {
 					continue
--- samples/ruby/app.rb
+++ samples/ruby/app.rb
@@ -1,4 +1,4 @@
-#   Copyright 2020 Move2Kube authors
+#   Copyright IBM Corporation 2020, 2021
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
