0ec86ce82016f5d14247ff358d8016d0ebc83382
Pablo Loyola <pablo.loyola.h@gmail.com>
2021-07-01 18:53:23 +0900

    refactored version of transformer for spring boot
    Signed-off-by: Pablo Loyola <pablo.loyola.h@gmail.com>

--- internal/transformer/classes/analysers/springbootanalyser.go
+++ internal/transformer/classes/analysers/springbootanalyser.go
@@ -25,20 +25,23 @@ import (
 	"github.com/creekorful/mvnparser"
 	"github.com/konveyor/move2kube/environment"
 	"github.com/konveyor/move2kube/internal/common"
-
 	irtypes "github.com/konveyor/move2kube/types/ir"
 	plantypes "github.com/konveyor/move2kube/types/plan"
+	springboot "github.com/konveyor/move2kube/types/source/springboot"
 	transformertypes "github.com/konveyor/move2kube/types/transformer"
 	"github.com/konveyor/move2kube/types/transformer/artifacts"
 	"github.com/sirupsen/logrus"
 )
+
+const pomXML string = "pom.xml"
 
 const (
 	SpringbootServiceConfigType transformertypes.ConfigType = "SpringbootService"
 )
 
 const (
-	mavenPomXML transformertypes.PathType = "MavenPomXML"
+	mavenPomXML         transformertypes.PathType = "MavenPomXML"
+	applicationFilePath transformertypes.PathType = "SpringbootApplicationFile"
 )
 
 // SpringbootAnalyser implements Transformer interface
@@ -49,27 +52,12 @@ type SpringbootAnalyser struct {
 
 type SpringbootConfig struct {
 	ServiceName string `yaml:"serviceName,omitempty"`
+	Ports       []int  `yaml:"ports,omitempty"`
 }
 
 type SpringbootTemplateConfig struct {
-	Ports       []int
+	Port        int
 	JavaVersion string
-}
-
-type Application struct {
-	Name string `yaml:"name"`
-}
-type SpringSpec struct {
-	Application Application `yaml:"application"`
-}
-
-type SpringBootSpec struct {
-	SpringSpec SpringSpec `yaml:"spring"`
-	ServerSpec ServerSpec `yaml:"server"`
-}
-
-type ServerSpec struct {
-	Port int `yaml:"port"`
 }
 
 func (t *SpringbootAnalyser) Init(tc transformertypes.Transformer, env environment.Environment) (err error) {
@@ -83,72 +71,110 @@ func (t *SpringbootAnalyser) GetConfig() (transformertypes.Transformer, environm
 }
 
 func (t *SpringbootAnalyser) BaseDirectoryDetect(dir string) (namedServices map[string]plantypes.Service, unnamedServices []plantypes.Transformer, err error) {
-
 	return nil, nil, nil
 }
 
 func (t *SpringbootAnalyser) DirectoryDetect(dir string) (namedServices map[string]plantypes.Service, unnamedServices []plantypes.Transformer, err error) {
-
 	destEntries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		logrus.Errorf("Unable to process directory %s : %s", dir, err)
-	} else {
-		for _, de := range destEntries {
-			if de.Name() == "pom.xml" {
-
-				// filled with previously declared xml
-				pomStr, err := ioutil.ReadFile(filepath.Join(dir, de.Name()))
-				if err != nil {
-					return nil, nil, err
-				}
-
-				// Load project from string
-				var project mvnparser.MavenProject
-				if err := xml.Unmarshal([]byte(pomStr), &project); err != nil {
-					logrus.Errorf("unable to unmarshal pom file. Reason: %s", err)
-					return nil, nil, err
-				}
-
-				// Dont process if this is a root pom and there are submodules
-				if len(project.Modules) != 0 {
-					return nil, nil, nil
-				}
-
-				// Check if at least there is one springboot dependency
-				isSpringboot := false
-				for _, dependency := range project.Dependencies {
-					if strings.Contains(dependency.GroupId, "org.springframework.boot") {
-						isSpringboot = true
-					}
-				}
-				if !isSpringboot {
-					return nil, nil, nil
-				}
-
-				ct := plantypes.Transformer{
-					Mode:                   plantypes.ModeContainer,
-					ArtifactTypes:          []transformertypes.ArtifactType{irtypes.IRArtifactType, artifacts.ContainerBuildArtifactType},
-					ExclusiveArtifactTypes: []transformertypes.ArtifactType{artifacts.ContainerBuildArtifactType},
-					Configs: map[transformertypes.ConfigType]interface{}{
-						SpringbootServiceConfigType: SpringbootConfig{
-							ServiceName: filepath.Base(dir),
-						}},
-					Paths: map[transformertypes.PathType][]string{
-						mavenPomXML: {
-							filepath.Join(dir, "pom.xml"),
-						},
-						artifacts.ProjectPathPathType: {dir},
-					},
-				}
-				return map[string]plantypes.Service{filepath.Base(dir): []plantypes.Transformer{ct}}, nil, nil
-			}
+		return nil, nil, err
+	}
+	pomFound := false
+	for _, de := range destEntries {
+		if de.Name() == pomXML {
+			pomFound = true
+			break
 		}
 	}
-	return nil, nil, nil
+
+	if !pomFound {
+		return nil, nil, nil
+	}
+
+	// filled with previously declared xml
+	pomStr, err := ioutil.ReadFile(filepath.Join(dir, pomXML))
+	if err != nil {
+		logrus.Errorf("Could not read the pom.xml file", err)
+		return nil, nil, err
+	}
+
+	// Load project from string
+	var project mvnparser.MavenProject
+	if err := xml.Unmarshal([]byte(pomStr), &project); err != nil {
+		logrus.Errorf("unable to unmarshal pom file. Reason: %s", err)
+		return nil, nil, err
+	}
+
+	// Dont process if this is a root pom and there are submodules
+	if len(project.Modules) != 0 {
+		logrus.Debugf("Ignoring pom at %s as it has modules", dir)
+		return nil, nil, nil
+	}
+
+	// Check if at least there is one springboot dependency
+	isSpringboot := false
+	for _, dependency := range project.Dependencies {
+		if strings.Contains(dependency.GroupId, "org.springframework.boot") {
+			isSpringboot = true
+		}
+	}
+	if !isSpringboot {
+		logrus.Debugf("Ignoring pom at %s as does not contain Springboot dependencies: org.springframework.boot", dir)
+		return nil, nil, nil
+	}
+
+	appfiles, err := common.GetFilesByName(dir, []string{"application.yaml", "application.yml"})
+	if err != nil {
+		logrus.Debugf("Cannot get application files: %s", err)
+	}
+
+	validSpringbootFiles := []string{}
+
+	appName := filepath.Base(dir)
+	ports := []int{}
+
+	for _, appfile := range appfiles {
+		var springApplicationYaml springboot.SpringApplicationYaml
+		err = common.ReadYaml(appfile, &springApplicationYaml)
+		if err != nil {
+			logrus.Debugf("Could not load application file %s", appfile)
+			continue
+		}
+		if (springApplicationYaml == springboot.SpringApplicationYaml{}) {
+			logrus.Debugf("No information found in application file %s", appfile)
+			continue
+		}
+		validSpringbootFiles = append(validSpringbootFiles, appfile)
+
+		if springApplicationYaml.Spring.SpringApplication.Name != "" {
+			appName = springApplicationYaml.Spring.SpringApplication.Name
+		}
+
+		if springApplicationYaml.Server.Port != 0 {
+			ports = append(ports, springApplicationYaml.Server.Port)
+		}
+	}
+
+	ct := plantypes.Transformer{
+		Mode:                   plantypes.ModeContainer,
+		ArtifactTypes:          []transformertypes.ArtifactType{irtypes.IRArtifactType, artifacts.ContainerBuildArtifactType},
+		ExclusiveArtifactTypes: []transformertypes.ArtifactType{artifacts.ContainerBuildArtifactType},
+		Configs: map[transformertypes.ConfigType]interface{}{
+			SpringbootServiceConfigType: SpringbootConfig{
+				ServiceName: appName,
+				Ports:       ports,
+			}},
+		Paths: map[transformertypes.PathType][]string{
+			mavenPomXML:                   {filepath.Join(dir, pomXML)},
+			artifacts.ProjectPathPathType: {dir},
+			applicationFilePath:           validSpringbootFiles,
+		},
+	}
+	return map[string]plantypes.Service{appName: []plantypes.Transformer{ct}}, nil, nil
 }
 
 func (t *SpringbootAnalyser) Transform(newArtifacts []transformertypes.Artifact, oldArtifacts []transformertypes.Artifact) ([]transformertypes.PathMapping, []transformertypes.Artifact, error) {
-
 	pathMappings := []transformertypes.PathMapping{}
 	for _, a := range newArtifacts {
 		if a.Artifact != artifacts.ServiceArtifactType {
@@ -207,30 +233,16 @@ func (t *SpringbootAnalyser) Transform(newArtifacts []transformertypes.Artifact,
 			logrus.Errorf("error:", err)
 		}
 
-		dir := a.Paths[artifacts.ProjectPathPathType][0]
-
-		// capture port and app name
-		var stc SpringbootTemplateConfig
-		yamlpaths, err := common.GetFilesByExt(dir, []string{".yaml", ".yml"})
-		if err != nil {
-			logrus.Errorf("Cannot get yaml files", err)
-
-		}
-		for _, path := range yamlpaths {
-			sb := SpringBootSpec{}
-			if err := common.ReadYaml(path, &sb); err != nil {
-				continue
-			}
-
-			stc.Ports = []int{sb.ServerSpec.Port}
-			break
+		port := 8080
+		if len(sConfig.Ports) > 0 {
+			port = sConfig.Ports[0]
 		}
 
 		pathMappings = append(pathMappings, transformertypes.PathMapping{
 			Type:           transformertypes.TemplatePathMappingType,
 			SrcPath:        outputPath,
 			DestPath:       filepath.Join(common.DefaultSourceDir, relSrcPath, "Dockerfile."+seConfig.ServiceName),
-			TemplateConfig: stc,
+			TemplateConfig: SpringbootTemplateConfig{Port: port},
 		}, transformertypes.PathMapping{
 			Type:     transformertypes.SourcePathMappingType,
 			SrcPath:  "",
