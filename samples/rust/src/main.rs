aa6bb4a9e4983b36a964ecb7ae8abfe8ff6a3736
K mehant <411843@student.nitandhra.ac.in>
2021-07-01 21:29:03 +0530

    modify rust server port for container env
    Signed-off-by: K mehant <411843@student.nitandhra.ac.in>

--- samples/rust/src/main.rs
+++ samples/rust/src/main.rs
@@ -15,7 +15,7 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
