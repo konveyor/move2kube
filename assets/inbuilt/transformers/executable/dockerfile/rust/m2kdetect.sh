dd5ea22761641a7f7dbf1ef9825075950db3a423
K mehant <411843@student.nitandhra.ac.in>
2021-07-01 21:10:53 +0530

    adapt rust transformer to new folder structure and update license
    Signed-off-by: K mehant <411843@student.nitandhra.ac.in>

--- assets/inbuilt/transformers/executable/dockerfile/rust/m2kdetect.sh
+++ assets/inbuilt/transformers/executable/dockerfile/rust/m2kdetect.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#   Copyright IBM Corporation 2020
+#   Copyright IBM Corporation 2021
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
 
 # Takes as input the source directory and returns error if it is not fit
 BASE_DIR=$1
