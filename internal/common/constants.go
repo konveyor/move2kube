36714f8f3acb3cb60ca0a19067241e799f6dc413
Ashok Pon Kumar <ashokponkumar@gmail.com>
2021-09-13 23:17:55 +0530

    fix: Fix service name renaming; Remove error on finding incorrect yamls
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

--- internal/common/constants.go
+++ internal/common/constants.go
@@ -161,4 +161,6 @@ var (
 	IgnoreEnvironment = false
 	// DefaultIgnoreDirRegexps specifies directory name regexes that would be ignored
 	DefaultIgnoreDirRegexps = []*regexp.Regexp{regexp.MustCompile("[.].*")}
+	// Characters not allowed in a DNS Name
+	disallowedDNSCharactersRegex = regexp.MustCompile(`[^a-z0-9\-]`)
 )
