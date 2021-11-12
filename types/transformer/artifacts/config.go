ff36bc064647e34ac152d06d808d6d34e44da768
Ashok Pon Kumar <ashokponkumar@gmail.com>
2021-11-12 19:03:50 +0530

    feat: Allow for custom config merge functions
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

---
+++ types/transformer/artifacts/config.go
@@ -0,0 +1,36 @@
+/*
+ *  Copyright IBM Corporation 2021
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package artifacts
+
+import (
+	"reflect"
+
+	"github.com/konveyor/move2kube/common"
+	"github.com/konveyor/move2kube/types/ir"
+	transformertypes "github.com/konveyor/move2kube/types/transformer"
+)
+
+var (
+	ConfigTypes map[string]reflect.Type
+)
+
+func init() {
+	configObjs := []transformertypes.Config{
+		new(ir.IR),
+	}
+	ConfigTypes = common.GetTypesMap(configObjs)
+}
