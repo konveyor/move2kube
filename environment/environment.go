6f21a4559a06cbf10aea433442c91b7375325f6d
seshapad <seshapad@in.ibm.com>
2021-11-18 03:04:44 +0530

    Fixed debug formatted print
    Signed-off-by: seshapad <seshapad@in.ibm.com>

--- environment/environment.go
+++ environment/environment.go
@@ -320,7 +320,7 @@ func (e *Environment) DownloadAndDecode(obj interface{}, downloadSource bool) in
 			return relPath, nil
 		}
 		if _, err := os.Stat(path); err != nil {
-			logrus.Debug("Path [%s] does not exist", path)
+			logrus.Debugf("Path [%s] does not exist", path)
 			return path, nil
 		}
 		outpath, err := e.Env.Download(path)
