bfec684df5bdcadf45bf798ed0ddcf32bd4d4b27
Ashok Pon Kumar <ashokponkumar@gmail.com>
2022-02-11 17:09:20 +0530

    fix: fix lint errors
    Signed-off-by: Ashok Pon Kumar <ashokponkumar@gmail.com>

--- environment/container/fileinfo.go
+++ environment/container/fileinfo.go
@@ -23,30 +23,37 @@ import (
 	"github.com/docker/docker/api/types"
 )
 
+// FileInfo implements fs.FileInfo interface
 type FileInfo struct {
 	stat types.ContainerPathStat
 }
 
+// Name implements fs.FileInfo interface
 func (f *FileInfo) Name() string { // base name of the file
 	return f.stat.Name
 }
 
+// Size implements fs.FileInfo interface
 func (f *FileInfo) Size() int64 { // length in bytes for regular files; system-dependent for others
 	return f.stat.Size
 }
 
+// Mode implements fs.FileInfo interface
 func (f *FileInfo) Mode() fs.FileMode { // file mode bits
 	return f.stat.Mode
 }
 
+// ModTime implements fs.FileInfo interface
 func (f *FileInfo) ModTime() time.Time { // modification time
 	return f.stat.Mtime
 }
 
+// IsDir implements fs.FileInfo interface
 func (f *FileInfo) IsDir() bool { // abbreviation for Mode().IsDir()
 	return f.stat.Mode.IsDir()
 }
 
+// Sys implements fs.FileInfo interface
 func (f *FileInfo) Sys() interface{} { // underlying data source (can return nil)
 	return nil
 }
