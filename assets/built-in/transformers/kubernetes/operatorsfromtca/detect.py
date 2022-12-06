c90ea9651dfcd746569ae72e735995c4b6318636
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2022-12-06 20:56:27 +0530

    fix: look for external services in CfApps file instead of the CfServices file
    Also change the python code to use the new input and output format.
    
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- assets/built-in/transformers/kubernetes/operatorsfromtca/detect.py
+++ assets/built-in/transformers/kubernetes/operatorsfromtca/detect.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#   Copyright IBM Corporation 2021
+#   Copyright IBM Corporation 2022
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,69 +13,65 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 
-import sys
 import os
 import json
 import yaml
+from pathlib import Path
 
 # Performs the detection of pom file and extracts service name
-def detect(inputPath):
-    print('detect start inputPath:', inputPath)
+def detect(input_dir):
     services = {}
-    with open(inputPath) as f:
-        detectInput = json.loads(f.read())
-        print('detectInput', detectInput)
-        for rootDir, _, fileList in os.walk(detectInput["InputDirectory"]):
-            for filePath in fileList:
+    print("input_dir", input_dir)
+    for rootDir, _, fileList in os.walk(input_dir):
+        for filePath in fileList:
+            print(
+                f"checking to see if the file {filePath} is a Move2Kube collect output"
+            )
+            if not filePath.endswith(".yaml"):
+                continue
+            fullFilePath = os.path.join(rootDir, filePath)
+            with open(fullFilePath) as f:
+                collectFile = yaml.safe_load(f)
+            # print("collectFile:", collectFile)
+            if (
+                "apiVersion" not in collectFile
+                or collectFile["apiVersion"] != "move2kube.konveyor.io/v1alpha1"
+            ):
                 print(
-                    "checking to see if the file",
-                    filePath,
-                    "is a Move2Kube collect output",
+                    f"apiVersion does not match. Expected: 'move2kube.konveyor.io/v1alpha1' Actual: {collectFile['apiVersion']}"
                 )
-                if not filePath.endswith(".yaml"):
-                    continue
-                fullFilePath = os.path.join(rootDir, filePath)
-                with open(fullFilePath) as fff:
-                    collectFile = yaml.safe_load(fff)
-                    # print("collectFile:", collectFile)
-                    if (
-                        "apiVersion" not in collectFile
-                        or collectFile["apiVersion"] != "move2kube.konveyor.io/v1alpha1"
-                    ):
-                        print(
-                            "apiVersion does not match. Expected: 'move2kube.konveyor.io/v1alpha1' Actual: {}".format(
-                                collectFile["apiVersion"]
-                            )
-                        )
-                        continue
-                    if "kind" not in collectFile or collectFile["kind"] != "CfServices":
-                        print(
-                            "kind does not match. Expected: 'CfServices' Actual: {}".format(
-                                collectFile["kind"]
-                            )
-                        )
-                        continue
-                    print("found a Move2Kube collect output YAML file")
-                    # print(collectFile)
-                    artifactName = os.path.basename(fullFilePath)
-                    services[artifactName] = [
-                        {
-                            "name": artifactName,
-                            "type": "CollectOutput",
-                            "paths": {"CollectOutput": [fullFilePath]},
-                        }
-                    ]
-    # print('detect end services:', services)
+                continue
+            if "kind" not in collectFile or (
+                collectFile["kind"] != "CfApps" and collectFile["kind"] != "CfServices"
+            ):
+                print(
+                    f"kind does not match. Expected: 'CfApps' or 'CfServices' Actual: {collectFile['kind']}"
+                )
+                continue
+            print("found a CfApps or CfServices collect output YAML file")
+            # print(collectFile)
+            artifactName = os.path.basename(fullFilePath)
+            services[artifactName] = [
+                {
+                    "name": artifactName,
+                    "type": "CollectOutput",
+                    "paths": {"CollectOutput": [fullFilePath]},
+                }
+            ]
     return services
 
 
 # Entry-point of detect script
 def main():
-    services = detect(sys.argv[1])
-    outDir = "/var/tmp/m2k_detect_output"
-    os.makedirs(outDir, exist_ok=True)
-    with open(os.path.join(outDir, "m2k_detect_output.json"), "w+") as f:
-        json.dump(services, f)
+    input_path = os.environ["M2K_DETECT_INPUT_PATH"]
+    with open(input_path) as f:
+        detectInput = json.load(f)
+    output = detect(detectInput["InputDirectory"])
+    output_path = os.environ["M2K_DETECT_OUTPUT_PATH"]
+    output_dir = Path(output_path).parent.absolute()
+    os.makedirs(output_dir, mode=0o777, exist_ok=True)
+    with open(output_path, "w") as f:
+        json.dump(output, f)
 
 
 if __name__ == "__main__":
