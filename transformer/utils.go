90a5e26914d91434d46a4e8f6cea5b888114b076
HarikrishnanBalagopal <harikrishmenon@gmail.com>
2023-01-17 16:12:42 +0530

    chore: better error messages and minor bug fixes (#948)
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- transformer/utils.go
+++ transformer/utils.go
@@ -34,28 +34,29 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 )
 
-func getTransformerConfig(path string) (tc transformertypes.Transformer, err error) {
-	tc = transformertypes.NewTransformer()
-	tc.Spec.FilePath = path
-	if err := common.ReadMove2KubeYaml(path, &tc); err != nil {
-		logrus.Debugf("Failed to read the transformer metadata at path %q Error: %q", path, err)
-		return tc, err
+func getTransformerConfig(transformerYamlPath string) (transformertypes.Transformer, error) {
+	tc := transformertypes.NewTransformer()
+	tc.Spec.TransformerYamlPath = transformerYamlPath
+	if err := common.ReadMove2KubeYaml(transformerYamlPath, &tc); err != nil {
+		return tc, fmt.Errorf("failed to read the transformer metadata from the yaml file at path '%s' . Error: %w", transformerYamlPath, err)
 	}
 	if tc.Kind != transformertypes.TransformerKind {
-		err := fmt.Errorf("the file at path %q is not a valid cluster metadata. Expected kind: %s Actual kind: %s", path, transformertypes.TransformerKind, tc.Kind)
-		logrus.Debug(err)
-		return tc, err
+		return tc, fmt.Errorf(
+			"the file at path '%s' is not a valid cluster metadata. Expected kind: '%s' Actual kind: '%s'",
+			transformerYamlPath, transformertypes.TransformerKind, tc.Kind,
+		)
 	}
 	if tc.Labels == nil {
 		tc.Labels = map[string]string{}
 	}
 	tc.Labels[transformertypes.LabelName] = tc.Name
+	var err error
 	if tc.Spec.OverrideSelector, err = getSelectorFromInterface(tc.Spec.Override); err != nil {
-		logrus.Errorf("Unable to parse override selector for %s, Ignoring selector : %+v", tc.Name, tc.Spec.Override)
+		logrus.Errorf("failed to parse the override selector for the transformer '%s' , Ignoring selector: %+v . Error: %q", tc.Name, tc.Spec.Override, err)
 		tc.Spec.OverrideSelector = nil
 	}
 	if tc.Spec.DependencySelector, err = getSelectorFromInterface(tc.Spec.Dependency); err != nil {
-		logrus.Errorf("Unable to parse dependency selector for %s, Ignoring selector : %+v", tc.Name, tc.Spec.Dependency)
+		logrus.Errorf("failed to parse the dependency selector for the transformer '%s' , Ignoring selector: %+v . Error: %q", tc.Name, tc.Spec.Dependency, err)
 		tc.Spec.DependencySelector = nil
 	}
 	// TODO: Add check for consistency between consumes and produces
@@ -69,8 +70,7 @@ func getSelectorFromInterface(sel interface{}) (labels.Selector, error) {
 	s := metav1.LabelSelector{}
 	err := common.GetObjFromInterface(sel, &s)
 	if err != nil {
-		logrus.Errorf("Unable to parse Override configuration, ignoring override : %s", err)
-		return nil, err
+		return nil, fmt.Errorf("failed to get a label selector object from the interface, ignoring override. Error: %w", err)
 	}
 	if len(s.MatchExpressions) != 0 || len(s.MatchLabels) != 0 {
 		return metav1.LabelSelectorAsSelector(&s)
@@ -81,13 +81,13 @@ func getSelectorFromInterface(sel interface{}) (labels.Selector, error) {
 func getIgnorePaths(inputPath string) (ignoreDirectories []string, ignoreContents []string) {
 	filePaths, err := common.GetFilesByName(inputPath, []string{common.IgnoreFilename}, nil)
 	if err != nil {
-		logrus.Warnf("Unable to fetch .m2kignore files at path %q Error: %q", inputPath, err)
+		logrus.Warnf("failed to fetch .m2kignore files at path '%s' . Error: %q", inputPath, err)
 		return ignoreDirectories, ignoreContents
 	}
 	for _, filePath := range filePaths {
 		file, err := os.Open(filePath)
 		if err != nil {
-			logrus.Warnf("Failed to open the .m2kignore file at path %q Error: %q", filePath, err)
+			logrus.Warnf("failed to open the .m2kignore file at path '%s' . Error: %q", filePath, err)
 			continue
 		}
 		defer file.Close()
@@ -230,24 +230,27 @@ func getNamedAndUnNamedServicesLogMessage(services map[string][]plantypes.PlanAr
 	return fmt.Sprintf("Identified %d named services and %d to-be-named services", nnservices, nuntransformers)
 }
 
-func getFilteredTransformers(transformerPaths map[string]string, selector labels.Selector, logError bool) (transformerConfigs map[string]transformertypes.Transformer) {
+func getFilteredTransformers(transformerYamlPaths map[string]string, selector labels.Selector, logError bool) map[string]transformertypes.Transformer {
 	filteredTransformerConfigs := map[string]transformertypes.Transformer{}
 	overrideSelectors := []labels.Selector{}
-	for tn, tfilepath := range transformerPaths {
-		tc, err := getTransformerConfig(tfilepath)
+	for transformerName, transformerYamlPath := range transformerYamlPaths {
+		tc, err := getTransformerConfig(transformerYamlPath)
 		if err != nil {
 			if logError {
-				logrus.Errorf("failed to load '%s' as a Transformer config . Error: %q", tfilepath, err)
+				logrus.Errorf("failed to load the YAML file at path '%s' as a Transformer config. Error: %q", transformerYamlPath, err)
 			} else {
-				logrus.Debugf("failed to load '%s' as a Transformer config . Error: %q", tfilepath, err)
+				logrus.Debugf("failed to load the YAML file at path '%s' as a Transformer config. Error: %q", transformerYamlPath, err)
 			}
 			continue
 		}
 		if ot, ok := filteredTransformerConfigs[tc.Name]; ok {
-			logrus.Errorf("Found two conflicting transformer Names %s : %s, %s. Ignoring %s.", tc.Name, ot.Spec.FilePath, tc.Spec.FilePath, ot.Spec.FilePath)
+			logrus.Warnf(
+				"Found two transformers with the same name '%s' at paths '%s' and '%s' . Ignoring the one at path '%s'",
+				tc.Name, ot.Spec.TransformerYamlPath, tc.Spec.TransformerYamlPath, ot.Spec.TransformerYamlPath,
+			)
 		}
 		if !selector.Matches(labels.Set(tc.Labels)) {
-			logrus.Debugf("Ignoring transformer %s because of filter", tn)
+			logrus.Debugf("Ignoring the transformer '%s' because its labels don't match the selector", transformerName)
 			continue
 		}
 		if tc.Spec.OverrideSelector != nil {
@@ -257,12 +260,15 @@ func getFilteredTransformers(transformerPaths map[string]string, selector labels
 			filteredTransformerConfigs[tc.Name] = tc
 			continue
 		}
-		logrus.Errorf("Ignoring transformer %s since the class %s not found", tn, tc.Spec.Class)
+		logrus.Errorf("Ignoring the transformer '%s' since the transformer class '%s' was not found", transformerName, tc.Spec.Class)
 	}
-	transformerConfigs = map[string]transformertypes.Transformer{}
-	for tn, tc := range filteredTransformerConfigs {
+	transformerConfigs := map[string]transformertypes.Transformer{}
+	for transformerName, tc := range filteredTransformerConfigs {
 		if ot, ok := transformerConfigs[tc.Name]; ok {
-			logrus.Errorf("Found two conflicting transformer Names %s : %s, %s. Ignoring %s.", tc.Name, ot.Spec.FilePath, tc.Spec.FilePath, ot.Spec.FilePath)
+			logrus.Warnf(
+				"Found two transformers with the same name '%s' at paths '%s' and '%s' . Ignoring the one at path '%s'",
+				tc.Name, ot.Spec.TransformerYamlPath, tc.Spec.TransformerYamlPath, ot.Spec.TransformerYamlPath,
+			)
 		}
 		ignore := false
 		for _, overrideSelector := range overrideSelectors {
@@ -272,7 +278,7 @@ func getFilteredTransformers(transformerPaths map[string]string, selector labels
 			}
 		}
 		if !ignore {
-			transformerConfigs[tn] = tc
+			transformerConfigs[transformerName] = tc
 		}
 	}
 	return transformerConfigs
