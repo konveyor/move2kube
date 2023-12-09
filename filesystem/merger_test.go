5b830377ea24a2a07bd2ee02ee412f6f4ab63362
satyazzz123 <beherasatyajit716@gmail.com>
2023-12-10 01:17:45 +0530

    test : add tests for mergeDeletionCallBack
    Signed-off-by: satyazzz123 <beherasatyajit716@gmail.com>

---
+++ filesystem/merger_test.go
@@ -0,0 +1,72 @@
+// package filesystem
+
+// import (
+// 	"os"
+// 	"testing"
+// )
+
+// func TestMergeDeletionCallBack(t *testing.T) {
+// 	t.Run("test for source file and destination directory", func(t *testing.T) {
+// 		source := "/move2kube/to/nonexistent/source"
+// 		destination := "/move2kube/to/destination"
+// 		config := interface{}(nil)
+
+// 		// Call the merge function
+// 		err := mergeDeletionCallBack(source, destination, config)
+
+// 		// Assert that the error is not nil (as the source file does not exist)
+// 		if err == nil {
+// 			t.Fatalf("Expected non-nil error, but got nil")
+// 		}
+
+// 		// Check if the error is due to the source file not existing
+// 		if !os.IsNotExist(err) {
+// 			t.Fatalf("Expected source file to not exist, but got error: %v", err)
+// 		}
+
+// 		// Additional checks based on the expected behavior after fixing the source path
+// 		// For example, check the state of the destination directory or other behavior
+// 		_, destErr := os.Stat(destination)
+// 		if destErr == nil {
+// 			t.Error("Expected destination directory not to exist, but it exists")
+// 		}
+
+// 		// Additional checks specific to your expected behavior can be added here
+// 	})
+// }
+package filesystem
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMergeDeletionCallBack(t *testing.T) {
+	t.Run(" test for scenario when function is called with a non-existent source file and an existing destination directory", func(t *testing.T) {
+		source := "/move2kube/nonexistent/source"
+		destination := "/move2kube/destination"
+		config := interface{}(nil)
+
+		// Call the merge function
+		err := mergeDeletionCallBack(source, destination, config)
+
+		// Assert that the error is not nil (as the source file does not exist)
+		if err == nil {
+			t.Fatalf("Expected non-nil error, but got nil")
+		}
+
+		// Check if the error is due to the source file not existing
+		if !os.IsNotExist(err) {
+			t.Fatalf("Expected source file to not exist, but got error: %v", err)
+		}
+
+		// Additional checks based on the expected behavior after fixing the source path
+		// For example, check the state of the destination directory or other behavior
+		_, destErr := os.Stat(destination)
+		if !os.IsNotExist(destErr) {
+			t.Error("Expected destination directory not to exist, but it exists")
+		}
+
+		// Additional checks specific to your expected behavior can be added here
+	})
+}
